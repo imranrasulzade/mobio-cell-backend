a7f488c27a2815d112f310229889adf0b8e67d9e
imranrasulzade <imranrasulzade5@gmail.com>
2025-06-18 01:00:46 +0400

    files add

---
+++ ms-common/src/main/java/com/mobiocell/mscommon/MsCommonApplication.java
@@ -0,0 +1,13 @@
+package com.mobiocell.mscommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCommonApplication.class, args);
+	}
+
+}
